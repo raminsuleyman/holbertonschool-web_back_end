22c7fdfbcfe1527b1c91c4b27534881495bb6547
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-07-09 04:25:12 +0000

    0

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Module for async generator."""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Yield 10 random numbers after 1 second asynchronously."""
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
