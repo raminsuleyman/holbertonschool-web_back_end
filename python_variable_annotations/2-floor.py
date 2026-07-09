3da6c282fe5ff0aa58278907d34f9ad1409d9d50
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-07-09 03:57:02 +0000

    2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module that contains a typed floor function."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float number."""
+    return math.floor(n)
