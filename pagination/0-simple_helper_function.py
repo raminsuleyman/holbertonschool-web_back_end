64f907e18b050b2ee687d015e877d571fed29611
raminsuleyman <ramin.suleymanvv@icloud.com>
2026-08-05 10:15:54 +0000

    0

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -2,9 +2,8 @@
 """Pagination helper functiuon"""
 
 
-
-def index_range(page: int, page_size:int) -> tuple:
+def index_range(page: int, page_size: int) -> tuple:
     """"Return a tuple containing the start and end indexes"""
-    start_index =(page-1) * page_size
+    start_index = (page-1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
